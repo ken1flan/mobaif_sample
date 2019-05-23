7ffa95514e57b4412fc573f3613d5f94d0262184
ken1flan <ken1flan@gmail.com>
2019-05-23 20:17:01 +0900

    Fix createdb.sql
    create tableのオプションがtypeから
    engineに変更になっていた。

--- conf/createdb.sql
+++ conf/createdb.sql
@@ -15,13 +15,13 @@ create table user_data (
   reg_date      int         unsigned not null, # 入会日時
   user_st       tinyint              not null, # ユーザステータス
   serv_st       tinyint              not null, # サービスステータス
-  
+
   carrier       char(1)              not null, # キャリア ( D | A | V )
   model_name    varchar(20)          not null, # 現在の機種名
   subscr_id     varchar(40)                  , # サブスクライバID
   serial_id     varchar(30)                    # SIMカード / 端末ID
 
-) type=InnoDB;
+) engine=InnoDB;
 
 alter table user_data
  add primary key     (user_id),
