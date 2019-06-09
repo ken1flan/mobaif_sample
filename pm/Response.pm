1fa34249b30268d17e76816ef8bd7d7d3ee9c9cf
ken1flan <ken1flan@gmail.com>
2019-06-10 07:25:35 +0900

    キャッシュのコントロールを削除

--- pm/Response.pm
+++ pm/Response.pm
@@ -29,24 +29,6 @@ sub output {
 	my $len = length($html);
 	print "Content-length: $len\r\n";
 
-	# FOMA では、no-cache を指定すると、ブラウザバックの場合でも
-	# 再読み込みされるので、このような挙動にしている。
-
-	# au はキャッシュが強いので、必ず no-cache を付ける。
-	# no-cache を付けても、進む／戻るではキャッシュした
-	# ページが使われるのでうざくない。
-
-	my $nocache =
-		($cache eq 'no-cache') ? 1 :
-		($cache eq 'cache')    ? 0 :
-		($ENV{REQUEST_METHOD} eq 'POST') ? 0 :
-		($ENV{MB_MODEL_TYPE}  eq 'DF'  ) ? 0 : 1;
-
-	if ($nocache) {
-		print "Cache-control: no-cache\r\n";
-		print "Pragma: no-cache\r\n";
-	}
-
 	print "Connection: close\r\n";
 	print "\r\n$html";
 	MLog::write("$_::LOG_DIR/debug", "Response::ouput() end");
