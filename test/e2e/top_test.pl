ad8c29e1f2e203add74a2448c570c2445a0ca908
ken1flan <ken1flan@gmail.com>
2019-11-29 07:23:51 +0900

    Add e2e test
    トップページを表示するテストを追加。
    設定などをまとめる作業はしていない。

---
+++ test/e2e/top_test.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+use utf8;
+
+use Log::Log4perl qw(:easy);
+use WWW::Mechanize::Chrome;
+
+use Test::Simple tests => 1;
+
+my $mech = WWW::Mechanize::Chrome->new(headless=> 1);
+$mech->get('http://127.0.0.1');
+my $content = $mech->content;
+
+ok( $content =~ /トップページ/ );
