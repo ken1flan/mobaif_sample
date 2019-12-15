9e97bd3177e8533936468378fbf4770c7f43ef8e
ken1flan <ken1flan@gmail.com>
2019-12-16 07:51:30 +0900

    Add user registration
    /user/new よりユーザ登録を行えるようにした。

--- pm/Page/User.pm
+++ pm/Page/User.pm
@@ -6,6 +6,7 @@ use Common;
 use HTMLTemplate;
 use Response;
 use DA;
+use Func::User;
 
 sub pageShow {
 	my $func = shift;
@@ -17,6 +18,7 @@ sub pageShow {
 
 sub pageNew {
 	my $func = shift;
+	my $func = shift;
 	my $rhData = {};
 
 	my $html = HTMLTemplate::insert("user/new", $rhData);
@@ -26,8 +28,20 @@ sub pageNew {
 sub pageCreate {
 	my $func = shift;
 	my $rhData = {};
+	Common::mergeHash($rhData, $_::F);
 
-	my $html = HTMLTemplate::insert("user/create", $rhData);
+	my $errors = Func::User::validate($rhData);
+	my @keys = keys %$errors;
+	my $html = "";
+	if (grep { /^Err/ } @keys) {
+		Common::mergeHash($rhData, $errors);
+		$html = HTMLTemplate::insert("user/new", $rhData);
+	} else {
+		Func::User::create($rhData);
+		$html = HTMLTemplate::insert("user/create", $rhData);
+		my $dbh = DA::getHandle($_::DB_USER_W);
+		$dbh->commit();
+	}
 	Response::output(\$html);
 }
 
